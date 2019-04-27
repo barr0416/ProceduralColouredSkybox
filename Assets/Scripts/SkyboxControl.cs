4509526bb1788df5b6ee875210013289d9002dd6
Cody Barr <codydbarr@gmail.com>
2019-04-27 13:34:11 -0400

    Added dynamic color changing for skybox and light
    - Can change light over time

--- Assets/Scripts/SkyboxControl.cs
+++ Assets/Scripts/SkyboxControl.cs
@@ -13,9 +13,12 @@ public class SkyboxControl : MonoBehaviour
     public bool randomColors = false;
     public float randomColorsSwitchTimer = 5.0f;
     private float lastSwitch = 0.0f;
+    private float lerpStep = 0.0f;
 
     public bool changeLightColor = true;
     public Light globalLight;
+
+    public bool lerpSkyboxColors = false;
 
     private void Start()
     {
@@ -28,11 +31,21 @@ public class SkyboxControl : MonoBehaviour
     {
         lastSwitch += Time.deltaTime;
 
-        if(randomColors && lastSwitch >= randomColorsSwitchTimer)
+        if(randomColors && lastSwitch >= randomColorsSwitchTimer && lerpSkyboxColors)
+        {
+            StartCoroutine("TransitionColors");
+            lastSwitch = 0.0f;
+        }
+        else if(randomColors && lastSwitch >= randomColorsSwitchTimer)
         {
             this.SetSkyboxColors(Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
             lastSwitch = 0.0f;
         }
+    }
+
+    private void LerpSkyColors()
+    {
+
     }
 
     private void SetSkyboxColors(Color bottomColor, Color topColor)
@@ -43,6 +56,40 @@ public class SkyboxControl : MonoBehaviour
         if(changeLightColor)
         {
             globalLight.color = topColor;
+        }
+    }
+
+    private IEnumerator TransitionColors()
+    {
+        Color oldTopColor = skyboxMaterial.GetColor("_Color2");
+        Color oldBottomColor = skyboxMaterial.GetColor("_Color1");
+
+        Color newTopColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+        Color newBottomColor = Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f);
+
+        while(lerpStep < 1)
+        {
+            skyboxMaterial.SetColor("_Color1", Color.Lerp(oldBottomColor, newBottomColor, lerpStep));
+            skyboxMaterial.SetColor("_Color2", Color.Lerp(oldTopColor, newTopColor, lerpStep));
+
+            globalLight.color = Color.Lerp(oldTopColor, newTopColor, lerpStep);
+            
+            lerpStep += Time.deltaTime * randomColorsSwitchTimer;
+
+            yield return null;
+        }
+
+        lerpStep = 0;
+    }
+
+    private void TransitionSkyboxColors()
+    {
+        skyboxMaterial.SetColor("_Color1", Color.Lerp(skyboxMaterial.GetColor("_Color1"), Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), randomColorsSwitchTimer));
+        skyboxMaterial.SetColor("_Color2", Color.Lerp(skyboxMaterial.GetColor("_Color2"), Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), randomColorsSwitchTimer));
+
+        if (changeLightColor)
+        {
+            //globalLight.color = topColor;
         }
     }
 }
