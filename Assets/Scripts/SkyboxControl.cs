208c37845383d87b4df264ceea961c41666bb5d1
Cody Barr <codydbarr@gmail.com>
2019-04-27 13:04:13 -0400

    Added color control script
    - Can control color through CS script in inspector
    - Can switch between colors at random
    - Can give specific colors
    - Can set to change a given lights color in the world

---
+++ Assets/Scripts/SkyboxControl.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyboxControl : MonoBehaviour 
+{
+    private Material skyboxMaterial;
+    public Skybox skybox;
+
+    public Color skyboxColorTop;
+    public Color skyboxColorBottom;
+
+    public bool randomColors = false;
+    public float randomColorsSwitchTimer = 5.0f;
+    private float lastSwitch = 0.0f;
+
+    public bool changeLightColor = true;
+    public Light globalLight;
+
+    private void Start()
+    {
+        skyboxMaterial = skybox.material;
+
+        this.SetSkyboxColors(skyboxColorBottom, skyboxColorTop);
+    }
+
+    private void Update()
+    {
+        lastSwitch += Time.deltaTime;
+
+        if(randomColors && lastSwitch >= randomColorsSwitchTimer)
+        {
+            this.SetSkyboxColors(Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f), Random.ColorHSV(0f, 1f, 1f, 1f, 0.5f, 1f));
+            lastSwitch = 0.0f;
+        }
+    }
+
+    private void SetSkyboxColors(Color bottomColor, Color topColor)
+    {
+        skyboxMaterial.SetColor("_Color1", bottomColor);
+        skyboxMaterial.SetColor("_Color2", topColor);
+
+        if(changeLightColor)
+        {
+            globalLight.color = topColor;
+        }
+    }
+}
